d68734e39a0d0670ec1c44c838ff8054bf62ea32
Eric Widjaja <ericwidjaja@pursuit.org>
2020-12-07 21:30:25 -0500

    initial commit

---
+++ ColorSwatchesApp/ColorSwatchesVC.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ColorSwatchesApp
+//
+//  Created by Eric Widjaja on 12/7/20.
+//  Copyright © 2020 ericSwidjaja. All rights reserved.
+//
+
+import UIKit
+
+class ColorSwatchesVC: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
