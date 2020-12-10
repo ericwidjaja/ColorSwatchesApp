a13d63cc927afa550ecf817b79eaf950665bf3e8
Eric Widjaja <ericwidjaja@pursuit.org>
2020-12-09 22:07:56 -0500

    connect storyboard with 'plus' button and make function to show UIColorPickerVC

---
+++ ColorSwatchesApp/ColorCell.swift
@@ -0,0 +1,15 @@
+//
+//  ColorCell.swift
+//  ColorSwatchesApp
+//
+//  Created by Eric Widjaja on 12/9/20.
+//  Copyright © 2020 ericSwidjaja. All rights reserved.
+//
+
+import UIKit
+
+class ColorCell: UICollectionViewCell {
+    
+    @IBOutlet weak var colorImageView: UIImageView! //Step 3.C
+    
+}
--- ColorSwatchesApp/ColorSwatchesVC.swift
+++ ColorSwatchesApp/ColorSwatchesVC.swift
@@ -1,10 +1,7 @@
-//
 //  ViewController.swift
 //  ColorSwatchesApp
-//
 //  Created by Eric Widjaja on 12/7/20.
 //  Copyright © 2020 ericSwidjaja. All rights reserved.
-//
 
 import UIKit
 
@@ -12,21 +9,45 @@ import UIKit
 class ColorSwatchesVC: UIViewController {
     
 //MARK: - Properties
-    private var colorPicker = UIColorPickerViewController()
+    private var selectedColor = UIColor.systemOrange        //Step 4.a
+    private var colorPicker = UIColorPickerViewController() //Step 4.a
     
 //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.colorPicker.delegate = self                    //Step 4.b
+        self.colorPicker.supportsAlpha = true               //Step 4.d
+        self.colorPicker.selectedColor = self.selectedColor //Step 4.d
+        self.view.backgroundColor = self.selectedColor      //Step 4.d
 
     }
     
 //MARK: - Methods
-    @IBAction func addColorButtonPressed(_ sender: UIBarButtonItem) {
+    @IBAction func addColorButtonPressed(_ sender: UIBarButtonItem) { //Step 4.e
+        present(self.colorPicker, animated: true, completion: nil)
     }
-    
 }
 
 //MARK: - Extensions
+@available(iOS 14.0, *)
+extension ColorSwatchesVC: UIColorPickerViewControllerDelegate {    //Step 4.c
+    func colorPickerViewControllerDidSelectColor(_ viewController: UIColorPickerViewController) {
+        self.selectedColor = viewController.selectedColor
+    }
+    
+    func colorPickerViewControllerDidFinish(_ viewController: UIColorPickerViewController) {
+        self.view.backgroundColor = self.selectedColor
+    }
+}
 
-
+/*
+ 1. on Main.storyboard set up:
+    a. UICollectionView, pin an ImageView to all its cell's edges
+    b. named the cell, in reuseIdentifier: 'ColorCell"
+ 
+ 2. Create a 'ColorCell.swift' as a Cocoa Touch Class file, subclasses of UICollectionViewCell
+ 3. Connect MainStoryBoard:
+    a. change UICollectionViewCell's name in attribute inspector to 'ColorCell',
+    b. make sure the ReuseIdentifier is saying the same -> 'ColorCell'
+    c. connect the ImageView to ColorCell Class file
+ */
